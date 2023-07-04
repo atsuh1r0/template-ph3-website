ef734f636aba55dd6f332fa17c76288ccc521173
Atsuhiro Nakai <106391644+atsuh1r0@users.noreply.github.com>
2023-07-04 21:14:48 +0900

    add: week2終了

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = User::all();
+        return view('user.index', compact('users'));
+        // return view('user.index', ['users' => $users]);
+    }
 }
